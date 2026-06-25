d39be1b1bb8900c5bf394f9328500744d68e6ed3
vothaingoc <vothaingoc1991@gmail.com>
2026-06-25 19:24:40 +0900

    Store Sanity Studio deployment app id

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,5 +4,8 @@ export default defineCliConfig({
   api: {
     projectId: process.env.PUBLIC_SANITY_PROJECT_ID || '',
     dataset: process.env.PUBLIC_SANITY_DATASET || 'production'
+  },
+  deployment: {
+    appId: 't6dy2wwadrj9q1jeh0enjm3b'
   }
 });
