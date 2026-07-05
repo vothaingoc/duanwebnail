a319f055cfc27751f8a6263612718a381a54249b
vothaingoc <vothaingoc1991@gmail.com>
2026-07-05 09:45:45 +0900

    Add SEO sitemap and robots files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://golynnail.jp',
   output: 'static',
-  trailingSlash: 'always'
+  trailingSlash: 'always',
+  integrations: [
+    sitemap()
+  ]
 });
